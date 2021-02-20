3741feca3b08f8e24fcf314a2f44a5a81bba9836
KingWither <67877704+KingWither@users.noreply.github.com>
2021-02-20 17:41:08 -0500

    hello

--- src/js/map.js
+++ src/js/map.js
@@ -23,7 +23,8 @@ window.onload = async function () {
     document.querySelector("#map").insertAdjacentHTML('afterbegin', map);
     document.querySelector("#hemicircle").insertAdjacentHTML('afterbegin', hemicircle);
 
-    let totals = [];
+    let totals = {};
+    let party_list = [];
 
     document.querySelectorAll("#map path").forEach(async function (path, n) {
         let call = await fetch('data/' + year + '/' + path.id.replace("-", " ").replace("-", " ") + '.json');
@@ -31,7 +32,6 @@ window.onload = async function () {
             const parties = (await (call).json())["parties"];
 
             let state_circles = Array.from(document.querySelectorAll('#map ellipse.' + path.id));
-            let national_circles = Array.from(document.querySelectorAll('#hemicircle circle'));
 
             state_circles.sort((a, b) => (
                 (parseFloat(a.getAttribute("cy")) > parseFloat(b.getAttribute("cy"))) ||
@@ -43,7 +43,28 @@ window.onload = async function () {
                     state_circles[0].style.fill = party_colors[party.name];
                     state_circles.shift();
                 }
+                try {
+                    totals[party.name]['seats'] += party.seats;
+                    totals[party.name]['votes'] += party.votes;
+                } catch {
+                    totals[party.name] = {
+                        'name': party.name,
+                        'seats': 0,
+                        'votes': 0
+                    };
+                    party_list.push(party.name);
+                    totals[party.name]['seats'] = party.seats;
+                    totals[party.name]['votes'] = party.votes;
+                }
             });
         }
     });
+
+    let national_circles = Array.from(document.querySelectorAll('#hemicircle circle'));
+    console.log(party_list);
+    party_list.forEach(function (party, n) {
+        for (let c = 0; c < totals[party].seats; c++) {
+            national_circles[c].style.fill = party_colors[totals[party].name];
+        }
+    })
 }
