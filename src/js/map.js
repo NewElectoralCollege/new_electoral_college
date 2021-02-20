6c6ad4fe948eb350daef9305fd2443c2bf595444
KingWither <67877704+KingWither@users.noreply.github.com>
2021-02-20 14:47:00 -0500

    Updating files

--- src/js/map.js
+++ src/js/map.js
@@ -2,9 +2,6 @@ const roundTen = function (n) {
     return Math.floor(n / 10) * 10;
 };
 
-const college_sizes = {
-    2020: "538"
-};
 
 window.onload = async function () {
     let year;
@@ -17,7 +14,7 @@ window.onload = async function () {
 
     const census = roundTen(year - 1);
     const map = await (await fetch('src/img/maps/us_electoral_college_' + census + '.svg')).text();
-    const hemicircle = await (await fetch('src/img/hemicircles/' + college_sizes[year] + '_seats_electoral_college.svg')).text();
+    const hemicircle = await (await fetch('src/img/hemicircles/538_seats_electoral_college.svg')).text();
     document.querySelector("#map").insertAdjacentHTML('afterbegin', map);
     document.querySelector("#hemicircle").insertAdjacentHTML('afterbegin', hemicircle);
 }
