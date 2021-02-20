6755a432c71bf44c5342f0673e726e3edc4d12d4
KingWither <67877704+KingWither@users.noreply.github.com>
2021-02-20 17:31:23 -0500

    start

--- src/js/map.js
+++ src/js/map.js
@@ -2,6 +2,12 @@ const roundTen = function (n) {
     return Math.floor(n / 10) * 10;
 };
 
+const party_colors = {
+    "Democratic": "#3333ff",
+    "Republican": "#ff3333",
+    "Libertarian": "#000000"
+}
+
 window.onload = async function () {
     let year;
     try {
@@ -17,8 +23,27 @@ window.onload = async function () {
     document.querySelector("#map").insertAdjacentHTML('afterbegin', map);
     document.querySelector("#hemicircle").insertAdjacentHTML('afterbegin', hemicircle);
 
-    document.querySelectorAll("#" + census + " path").forEach(async function (path, n) {
-        const call = await fetch('data/' + year + '/' + path.id + '.json');
-        console.log(call);
+    let totals = [];
+
+    document.querySelectorAll("#map path").forEach(async function (path, n) {
+        let call = await fetch('data/' + year + '/' + path.id.replace("-", " ").replace("-", " ") + '.json');
+        if (call.status == 200) {
+            const parties = (await (call).json())["parties"];
+
+            let state_circles = Array.from(document.querySelectorAll('#map ellipse.' + path.id));
+            let national_circles = Array.from(document.querySelectorAll('#hemicircle circle'));
+
+            state_circles.sort((a, b) => (
+                (parseFloat(a.getAttribute("cy")) > parseFloat(b.getAttribute("cy"))) ||
+                (parseFloat(a.getAttribute("cy")) == parseFloat(b.getAttribute("cy")) && parseFloat(a.getAttribute("cx")) > parseFloat(b.getAttribute("cx")))
+            ) ? 1 : -1);
+
+            parties.forEach(function (party, i) {
+                for (let c = 0; c < party.seats; c++) {
+                    state_circles[0].style.fill = party_colors[party.name];
+                    state_circles.shift();
+                }
+            });
+        }
     });
 }
