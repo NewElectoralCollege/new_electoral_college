0d79153221c71e9c2729b86ccec9af82ede8556c
KingWither <67877704+KingWither@users.noreply.github.com>
2021-03-02 20:41:21 -0500

    Added Nominees for 1976-2000

--- src/js/src/Nominees.elm
+++ src/js/src/Nominees.elm
@@ -1,4 +1,4 @@
-module Nominees exposing (..)
+module Nominees exposing (nominees)
 
 import Html exposing (..)
 import Dict exposing (..)
@@ -17,5 +17,52 @@ nominees =
             ("Libertarian", "Gary Johnson"),
             ("Green", "Jill Stein"),
             ("Independent", "Evan McMullin")
+        ]),
+        ( 2012, Dict.fromList [ 
+            ("Democratic", "Barack Obama"),
+            ("Republican", "Mitt Romney"),
+            ("Libertarian", "Gary Johnson")
+        ]),
+        ( 2008, Dict.fromList [ 
+            ("Democratic", "Barack Obama"),
+            ("Republican", "John McCain")
+        ]),
+        ( 2004, Dict.fromList [   
+            ("Republican", "George W. Bush"),
+            ("Democratic", "John Kerry")
+        ]),
+        ( 2000, Dict.fromList [   
+            ("Republican", "George W. Bush"),
+            ("Democratic", "Al Gore"),
+            ("Green", "Ralph Nader")
+        ]),
+        ( 1996, Dict.fromList [   
+            ("Democratic", "Bill Clinton"),
+            ("Republican", "Bob Dole"),
+            ("Reform", "Ross Perot"),
+            ("Green", "Ralph Nader")
+        ]),
+        ( 1992, Dict.fromList [   
+            ("Democratic", "Bill Clinton"),
+            ("Republican", "George H. W. Bush"),
+            ("Ross Perot", "Ross Perot")
+        ]),
+        ( 1988, Dict.fromList [   
+            ("Republican", "George H. W. Bush"),
+            ("Democratic", "Michael Dukakis")
+        ]),
+        ( 1984, Dict.fromList [   
+            ("Republican", "Ronald Reagan"),
+            ("Democratic", "Walter Mondale")
+        ]),
+        ( 1980, Dict.fromList [   
+            ("Republican", "Ronald Reagan"),
+            ("Democratic", "Jimmy Carter"),
+            ("Independent", "John B. Anderson"),
+            ("Libertarian", "Edward E. Clark")
+        ]),
+        ( 1976, Dict.fromList [   
+            ("Democratic", "Jimmy Carter"),
+            ("Republican", "Gerald Ford")
         ])
     ]
