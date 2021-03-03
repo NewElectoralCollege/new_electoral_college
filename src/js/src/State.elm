4727213509334ad5737f4b9fe55c5b4b0bef9a63
KingWither <67877704+KingWither@users.noreply.github.com>
2021-03-02 19:28:15 -0500

    further updated State.elm

--- src/js/src/State.elm
+++ src/js/src/State.elm
@@ -2,26 +2,23 @@ module State exposing (..)
 
 import Html exposing (..)
 import Basics exposing (..)
+import Http exposing (..)
+import List exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
-year : Int
-year =
-    2020
-
-angle : Int
-angle =
-    0
-
-assigned : Int
-assigned =
-    0
-
-assigned : Int
-assigned =
-    0
+dropMaybe : Maybe a -> a
+dropMaybe x =
+    case x of
+       Just y -> y
+       Nothing -> Debug.todo "A Nothing variable sent through dropMaybe function"
 
 bar_coords : List Int
 bar_coords =
     [100, 370]
+
+type Msg
+    = Success (Result Http.Error String)
 
 type alias Party =
     { name : String
@@ -29,29 +26,30 @@ type alias Party =
     , votes : Int
     , extra_votes : Int
     , extra_seat : Bool
+    , color : String
     }
 
-getAngle : Int -> Int -> Float
+getAngle : Float -> Int -> Float
 getAngle total_seats assigned = 
-    (PI / total_seats * (assigned + total_seats + 0.5))
+    (pi / total_seats * ((toFloat assigned) + total_seats + 0.5))
 
-doParty : Party -> Int -> Int -> Int -> String
+doParty : Party -> Int -> Float -> Int -> String
 doParty party n total_seats assigned =
-    if n == 19 || assigned == total_seats then
+    if n == 19 || assigned == party.seats then
         ""
     else
         let
             angle = getAngle total_seats assigned
             coords = [
                 350 * (cos angle) + 450,
-                350 * (sin angle) + 375
-            ]
+                350 * (sin angle) + 375 ]
         in
-            text "
-                <circle
-                    cx=" ++ String.fromInt coords.head ++ 
-                    "cy=" ++ String.fromInt coords.tail ++
-                    "r='10'" ++
-                    "id='" ++ String.fromInt assigned ++ 
-                    "style='fill:#000000'" ++
-                "/>" ++ doParty party n total_seats (assigned + 1)
+            (circle [cx String.fromFloat (dropMaybe (head coords)), cy String.fromFloat (dropMaybe (head (reverse coords))), r "10"] []) ++ doParty party n total_seats (assigned + 1)
+
+getFile : String -> String -> Cmd Msg
+getFile year state =
+    Http.get 
+    { url = "data/" ++ year ++ "/" ++ state ++ ".json"
+    , expect = Http.expectString Success
+    }
+
