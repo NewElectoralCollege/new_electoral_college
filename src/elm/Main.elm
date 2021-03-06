27571bdcd38a29b9945c6aa46c8299fe2512d6b0
KingWither <67877704+KingWither@users.noreply.github.com>
2021-03-06 18:05:30 -0500

    Updated comments

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -207,3 +207,10 @@ ifIsEnter msg =
                     Decode.fail "some other key"
             )
 
+
+{-
+  <link rel="stylesheet" href="http://localhost/new_electoral_college/src/sass/style.css"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+-}
+
