2ea3aaa36f279847d6ac714e5d80c66ab5767d92
KingWither <67877704+KingWither@users.noreply.github.com>
2021-03-08 16:58:43 -0500

    Moved some functions to "Util.elm" for modulation

--- src/elm/Util.elm
+++ src/elm/Util.elm
@@ -1,12 +1,29 @@
-module Util exposing (dropMaybe, styleNum, stylePercent, fix_change)
+module Util exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Http exposing (..)
 import String exposing (..)
 import Maybe exposing (..)
 import Tuple exposing (..)
 import List exposing (..)
 import Regex exposing (..)
+import Json.Decode as Decode exposing (Decoder)
+
+type alias Stats =
+    { name : String
+    , total_seats : Int
+    , total_votes : Int
+    , gallagher_index : Float
+    }
+
+type alias Party =
+    { name : String
+    , seats : Int
+    , votes : Int
+    , extra_votes : Int
+    , extra_seat : Bool
+    }
 
 dropMaybe : Maybe a -> a
 dropMaybe x =
@@ -86,3 +103,50 @@ fix_change string =
         [ i [ class "increase" ] [ text "&#9650;" ], text (fix_string string) ]
     else 
         [ text "n/a" ]
+
+type alias Election =
+    { list : List Party
+    , stats : Stats
+    }
+
+type alias MModel =
+    Int
+
+type Msg
+    = SendRequestParty
+    | PartySuccess (Result Http.Error (List Party))
+    | SendRequestStats
+    | StatSuccess (Result Http.Error Stats)
+    | Open MModel
+
+newParty : Decoder Party
+newParty =
+    Decode.map5 Party
+        (Decode.field "name" Decode.string)
+        (Decode.field "seats" Decode.int)
+        (Decode.field "votes" Decode.int)
+        (Decode.field "extra_votes" Decode.int)
+        (Decode.field "extra_seat" Decode.bool)
+
+setStats : Decoder Stats
+setStats =
+    Decode.map4 Stats
+        (Decode.field "name" Decode.string)
+        (Decode.field "total_seats" Decode.int)
+        (Decode.field "total_votes" Decode.int)
+        (Decode.field "gallagher_index" Decode.float)
+
+partyMsg : Expect Msg
+partyMsg =
+    Http.expectJson PartySuccess (Decode.at["parties"] (Decode.list newParty))
+
+statsMsg : Expect Msg
+statsMsg =
+    Http.expectJson StatSuccess (Decode.at["stats"] setStats)
+
+getFile : Expect Msg -> Int -> String -> Cmd Msg
+getFile msg year state =
+    Http.get 
+    { url = "http://localhost/new_electoral_college/data/" ++ String.fromInt year ++ "/" ++ state ++ ".json"
+    , expect = msg
+    }
