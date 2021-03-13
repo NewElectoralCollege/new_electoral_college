4cf0c80ff9a23eb043dbcbbf21d099b7e2f68449
KingWither <67877704+KingWither@users.noreply.github.com>
2021-03-13 16:53:33 -0500

    Further developed "Map.elm"

--- src/elm/Map.elm
+++ src/elm/Map.elm
@@ -1,4 +1,4 @@
-module Map exposing (..)
+port module Map exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -109,27 +109,145 @@ makeState election state =
                 election.stats.total_seats
             )) colors
 
-makePartyRow : Party -> Model -> Html Msg
-makePartyRow party model =
-    tr 
+makePartyRow : Party -> Maybe Party -> Model -> Html Msg
+makePartyRow party previous_party model =
+    let
+        pp =
+            case previous_party of
+                Nothing ->
+                    Party party.name 0 0 0 False "#dddddd"
+                Just a ->
+                    a
+    in
+        tr 
+            []
+            [ td [ class "color", id <| replace " " "-" party.name, style "background-color" party.color ] []
+            , td [] [ text party.name ]
+            , td [] [ text <| getNominee model.year party.name ]
+            , td [] [ text <| styleNum party.votes ]
+            , td [] [ text <| stylePercent <| Basics.toFloat party.votes / Basics.toFloat model.current.total_votes ]
+            , td [] [ text <| String.fromInt party.seats ]
+            , td [] [ text <| String.fromInt pp.seats ]
+            , td [] <| fix_change <| "+" ++ (String.fromInt <| party.seats - pp.seats)
+            ]
+
+rewriteInstance : Instance -> List (List Party) -> List Stats -> Instance
+rewriteInstance before parties stats =
+    { before
+    | states = Dict.fromList <| List.indexedMap (\i state -> (state, Election (dropMaybe <| getAt i parties) (dropMaybe <| getAt i stats) )) (keys states) 
+    , total_votes = List.foldl (\n s -> s + n.total_votes) 0 stats
+    , list = List.reverse <| sortBy .seats <| List.foldl (
+        \n list -> 
+            List.append list 
+            [{ n 
+            | votes = (List.foldl (\p s -> s + p.votes) 0 <| List.filter (\p -> p.name == n.name) <| List.concat parties)
+            , seats = (List.foldl (\p s -> s + p.seats) 0 <| List.filter (\p -> p.name == n.name) <| List.concat parties)
+            }]
+        ) [] (uniqueBy .name <| List.concat parties)
+    }
+
+doYearRow : String -> String -> Election -> Election -> Html Msg
+doYearRow state partyname current previous =
+    let 
+        party = 
+            ( dropMaybe <| find (\n -> n.name == partyname) current.list
+            , find (\n -> n.name == replace "Reform" "Ross Perot" partyname) previous.list
+            )
+        popularVotePercent = Basics.toFloat (first party).votes / Basics.toFloat current.stats.total_votes
+    in
+    tr
         []
-        [ td [ class "color", style "background-color" (getColor party colors) ] []
-        , td [] [ text party.name ]
-        , td [] [ text <| getNominee model.year party.name ]
-        , td [] [ text <| styleNum party.votes ]
-        , td [] [ text <| stylePercent <| Basics.toFloat party.votes / Basics.toFloat model.total_votes ]
-        , td [] [ text <| String.fromInt party.seats ]   
+        [ td [] [ text state ]
+        , td [] [ text <| styleNum <| (first party).votes ]
+        , td [] [ text <| stylePercent <| popularVotePercent ]
+        , td [] (
+            case second party of
+                Nothing ->
+                    [ text "n/a" ]
+                _ ->
+                    fix_change <| "+" ++ (stylePercent <| popularVotePercent - (Basics.toFloat (dropMaybe <| second party).votes / Basics.toFloat previous.stats.total_votes))
+            )
+        , td [] <| getPartyProgressBar (first party) current (first party).color
+        , td [] (
+            case second party of
+                Nothing ->
+                    [ text "n/a" ]
+                _ ->
+                    fix_change <| "+" ++ (String.fromInt <| (first party).seats - (dropMaybe <| second party).seats)
+            )
         ]
 
+partyContainer : String -> Model -> Html Msg
+partyContainer party model =
+    td 
+        [ class "detailed-results-cell" ] 
+        [ p [] [ text (party ++ " Party") ]
+        , table 
+            [ class "detailed-results" ]
+            (( thead 
+                [ style "background-color" "#eaecf0" ] 
+                [ tr 
+                    []
+                    [ th [ rowspan 2 ] [ text "State" ]
+                    , th [ colspan 3 ] []
+                    , th [ colspan 2 ] []
+                    ]
+                , tr 
+                    []
+                    [ th [] [ text "Votes" ]
+                    , th [] [ text "%" ]
+                    , th [] [ text "+/-" ]
+                    , th [] [ text "Electors" ]
+                    , th [] [ text "+/-" ]
+                    ]
+                ]
+            ) :: (List.map 
+                    (\state -> (
+                        let
+                            current = dropMaybe <| Dict.get state model.current.states
+                            previous = dropMaybe <| Dict.get state model.previous.states
+                        in
+                            doYearRow state party current previous
+                    )) 
+                    (keys model.current.states)))
+    ] 
+
+{-
+There is a possibility, that when the flickering year is fixed, that this wont work. In that case:
+lastYear - 4
+firstYear + 4
+-}
+
+getArrow : String -> Model -> List (Attribute Msg)
+getArrow side model =
+    let
+        change =
+            case side of
+                "left" ->
+                    -4
+                _ ->
+                    4 
+    in
+        if (side == "left" && model.year == firstYear) || (side == "right" && model.year == lastYear) then
+            []
+        else
+            [ id (side ++ "Arrow"), onClick <| ChangeYear (model.year + change) False ]
+
 type Msg 
-    = ChangeYear (Int)
+    = ChangeYear (Int) (Bool)
     | Response (Result Http.Error (Dict String String))
+
+type alias Instance =
+    { states : Dict String Election
+    , list : List Party
+    , total_votes : Int
+    }
 
 type alias Model =
     { year : Int
-    , states : Dict String Election
-    , list : List Party
-    , total_votes : Int
+    , writingToPrevious : Bool
+    , current : Instance
+    , previous : Instance
     }
 
 getFile : Int -> Cmd Msg
@@ -142,7 +260,8 @@ getFile year =
 init : Int -> (Model, Cmd Msg)
 init year =
     let
-        r = update (ChangeYear year) <| Model year empty [] 0
+        emptyInstance = Instance empty [] 0
+        r = update (ChangeYear year False) <| Model year False emptyInstance emptyInstance
     in
         ( first r
         , second r 
@@ -164,21 +283,20 @@ view model =
             [ class "container" ]
             [ div
                 [ class "container" ]
-                [ Html.span [ id "leftArrow", onClick <| ChangeYear (model.year - 4) ] []
+                [ Html.span (getArrow "left" model) []
                 , div 
                     [ class "container col-sm-4", id "map", style "display" "inline-block" ]
                     [ svg 
                         [ Sa.width "400mm", Sa.height "200mm", Sa.viewBox "0 0 800 193" ] 
                         (
-                            List.concatMap (\n -> makeState (dropMaybe <| Dict.get n model.states) n) (keys model.states)
+                            List.concatMap (\n -> makeState (dropMaybe <| Dict.get n model.current.states) n) (keys model.current.states)
                         )
                     ]
-                , Html.span [ id "rightArrow", onClick <| ChangeYear (model.year + 4) ] []
+                , Html.span (getArrow "right" model) []
                 ]
             , div
-                [ class "container", style "display" "inline-block" ]
-                [ div [ id "state-name" ] []
-                , div [ class "container col-sm-2", id "hemicircle", style "display" "inline-block" ] []
+                [ class "container" ]
+                [ div [ class "container col-sm-2", id "hemicircle", style "display" "inline-block" ] []
                 , div 
                     [ class "container col-sm-2", style "display" "inline-block", style "vertical-align" "middle", style "left" "360px" ]
                     [ table 
@@ -189,15 +307,28 @@ view model =
                             , th [ rowspan 2 ] [ text "Nominee" ]
                             , th [ colspan 2, rowspan 2 ] [ text "Votes" ]
                             , th [ colspan 2 ] [ text "Electors" ]
-                            , th [ rowspan 2 ] [ text "Change (New)" ]
+                            , th [ rowspan 2 ] [ text "Change" ]
                             ]
                         , tr
                             []
                             [ th [] [ text "New" ]
                             , th [] [ text "Old" ]
                             ]
-                        ] ++ (List.map (\n -> makePartyRow n model) <| takeWhile (\n -> ifQualifyingParty n <| Basics.toFloat model.total_votes) model.list))
+                        ] ++ (
+                            List.map 
+                                (\n -> makePartyRow n (find (\p -> p.name == n.name) model.previous.list) model) 
+                                <| takeWhile (\n -> ifQualifyingParty n <| Basics.toFloat model.current.total_votes) model.current.list))
                     ]
+                ]
+            , br [] []
+
+            , table 
+                [ class "container" ]
+                [ tr 
+                    [ id "row-for-detailed-results" ] 
+                    [ partyContainer "Democratic" model
+                    , partyContainer "Republican" model 
+                    ] 
                 ]
             ]
         ]
@@ -205,14 +336,14 @@ view model =
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        ChangeYear (int) ->
-            ({ model | year = int }, getFile int)
+        ChangeYear (year) (previous) ->
+            ({ model | writingToPrevious = previous, year = year }, getFile year)
         Response (Ok response) ->
             let
                 parties = List.map (\n ->
                     case Decode.decodeString (Decode.at["parties"] <| Decode.list newParty) <| dropMaybe <| Dict.get n response of
                         Ok (list) ->
-                            List.reverse <| sortBy .votes list
+                            List.reverse <| sortBy .votes <| List.map (\p -> { p | color = getColor p colors }) list
                         _ ->
                             []
                     ) (keys states)
@@ -224,19 +355,14 @@ update msg model =
                             Stats "" 0 0 0.0
                     ) (keys states)
             in
-                (
-                { model 
-                | states = Dict.fromList <| List.indexedMap (\i state -> (state, Election (dropMaybe <| getAt i parties) (dropMaybe <| getAt i stats) )) (keys states) 
-                , total_votes = List.foldl (\n s -> s + n.total_votes) 0 stats
-                , list = List.reverse <| sortBy .seats <| List.foldl (
-                    \n list -> 
-                        List.append list 
-                        [{ n 
-                        | votes = (List.foldl (\p s -> s + p.votes) 0 <| List.filter (\p -> p.name == n.name) <| List.concat parties)
-                        , seats = (List.foldl (\p s -> s + p.seats) 0 <| List.filter (\p -> p.name == n.name) <| List.concat parties)
-                        }]
-                ) [] (uniqueBy .name <| List.concat parties)
-                }, Cmd.none)
+                if model.writingToPrevious then
+                    ({ model | previous = rewriteInstance model.previous parties stats, writingToPrevious = False, year = model.year + 4 }, updateImages model.current.list)
+                else
+                    let
+                        tempmodel = { model | current = rewriteInstance model.current parties stats }
+                        r = update (ChangeYear (tempmodel.year - 4) True) tempmodel
+                    in
+                        (first r, second r)
         _ ->
             Debug.todo (Debug.toString msg)
 
@@ -248,3 +374,5 @@ main =
         , update = update
         , subscriptions = \_ -> Sub.none
         }
+
+port updateImages : List Party -> Cmd msg
