5468b0b65b1abfb20a68f814edb3e8f3393c99c1
KingWither <67877704+KingWither@users.noreply.github.com>
2021-03-15 18:55:46 -0400

    Futher developed "images.js"

--- src/js/images.js
+++ src/js/images.js
@@ -1,23 +1,18 @@
 window.onload = async function () {
     let app = Elm.Map.init({
         node: document.getElementById('main'),
-        flags: 2020
+        flags: Number(getCookie('year', 2020))
     });
 
     const hemicircle = await (await fetch('src/img/hemicircles/538_seats_electoral_college.svg')).text();
-    const map = await (await fetch('src/img/maps/us_electoral_college_2010.svg')).text();
-
-    const hemicircle_div = document.querySelector('#hemicircle');
-    const map_svg = document.querySelector("#map-svg");
 
     app.ports.updateImages.subscribe(function (parties) {
-    
-        console.log(map);
+        includeHTML();
+
+        const hemicircle_div = document.querySelector('#hemicircle');
         hemicircle_div.innerHTML = hemicircle;
-        map_svg.innerHTML += map;
         
         let circles = Array.from(document.querySelectorAll('g#Results circle'));
-
         parties.forEach(function (party, n) {
             for (let i = 0; i < party.seats; i++) {
                 circles[i].style.fill = party.color;
@@ -25,5 +20,11 @@ window.onload = async function () {
 
             circles.splice(0, party.seats);
         })
+    });
+
+    app.ports.wipeContent.subscribe(function (data) {
+        const main = document.querySelector('.include#map-svg');
+        main.removeChild(document.querySelector('#data'));
+        console.log("Hello");
     });
 }
