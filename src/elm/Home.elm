e9dfd3bb077e30eea7bc2f11fbc4c82913876949
KingWither <67877704+KingWither@users.noreply.github.com>
2021-03-24 16:38:58 -0400

    Updated main page

--- src/elm/Home.elm
+++ src/elm/Home.elm
@@ -1,6 +1,6 @@
 module Home exposing (..)
 
-import Html exposing (text, Html, div, h1, p, a, h2, hr)
+import Html exposing (text, Html, div, h1, p, a, h2, hr, b)
 import Html.Attributes exposing (class, href, attribute)
 import Browser exposing (element)
 import String exposing (fromChar)
@@ -33,6 +33,12 @@ view model =
                     [ text <| "We have a proposal to modify the Electoral College of the United States to make it more reflective of the voters. Instead " ++
                     "of all the electoral votes in a state going to the candidate that won the state's contest, the electors will be split proportionally."
                     ]
+                , p
+                    []
+                    [ text "We call this the "
+                    , b [] [ text "New Electoral College" ]
+                    , text "."
+                    ]
                 , p 
                     [] 
                     [ a
@@ -52,14 +58,39 @@ view model =
                     [ class "col-md-4" ]
                     [ h2
                         [ ]
-                        [ text "Heading" ]
+                        [ text "It's Fair" ]
                     , p
                         [ ]
-                        [ text "Here is some stuff" ]
+                        [ text "The New Electoral College will ensure that all votes matter, no matter who they are cast for, or where they are cast." ]
+                    ]
+                , div
+                    [ class "col-md-4" ]
+                    [ h2
+                        [ ]
+                        [ text "It's Ballanced" ]
+                    , p
+                        [ ]
+                        [ text <| "Candidates will only get the electoral votes that they deserve. Candidates will have to appeal to all voters, not just " ++
+                        "those who live in a couple of swing states."
+                        ]
+                    ]
+                , div
+                    [ class "col-md-4" ]
+                    [ h2
+                        [ ]
+                        [ text "It's Tested" ]
+                    , p
+                        [ ]
+                        [ text <| "Nearly 100 countries use Proportional Representation. It allows all factions to be represented, and prevents a monopoly " ++
+                        "on power by a single party."
+                        ]
                     ]
                 ]
             ]
         , hr [] []
+        , div
+            [ class "include container", attribute "w3-include-html" "src/img/electors_example.svg" ]
+            [ ]
         ]
 
 update : Msg -> Model -> (Model, Cmd Msg)
