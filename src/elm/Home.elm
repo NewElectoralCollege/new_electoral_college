43ebab79b88a0b89e65a36d2ecd79ba6b8cb2540
KingWither <67877704+KingWither@users.noreply.github.com>
2021-08-09 15:27:23 -0400

    Added images

--- src/elm/Home.elm
+++ src/elm/Home.elm
@@ -1,9 +1,35 @@
 module Home exposing (..)
 
 import Browser exposing (element)
-import Html exposing (Html, a, b, blockquote, br, div, footer, h1, h2, hr, p, text)
-import Html.Attributes exposing (attribute, class, href)
+import Html exposing (Html, a, b, blockquote, br, div, footer, h1, h2, hr, img, p, text)
+import Html.Attributes exposing (alt, attribute, class, href, src, style)
 import String exposing (fromChar)
+
+
+
+-- Image Sources
+
+
+license : String -> String -> String -> String -> Html Msg
+license image img_href photographer phg_href =
+    p
+        [ class "license" ]
+        [ a
+            [ href img_href ]
+            [ text <|
+                "\""
+                    ++ image
+                    ++ "\""
+            ]
+        , text " by "
+        , a
+            [ href phg_href ]
+            [ text <| photographer ]
+        , text " is liscensed under "
+        , a
+            [ href "https://creativecommons.org/licenses/by-sa/2.0/?ref=ccsearch&atype=html" ]
+            [ text "CC BY-SA 2.0" ]
+        ]
 
 
 
@@ -52,7 +78,7 @@ view model =
                     []
                     [ a
                         [ class "btn btn-primary btn-lg"
-                        , href "#"
+                        , href "./proposal.html"
                         , attribute "role" "button"
                         ]
                         [ text <| "Learn more " ++ fromChar '\u{00BB}' ]
@@ -74,7 +100,14 @@ view model =
                     ]
                 , div
                     [ class "col-md-4" ]
-                    [ h2
+                    [ img
+                        [ style "display" "block"
+                        , src "https://live.staticflickr.com/8600/15870725062_120c91470a_b.jpg"
+                        , alt "White House"
+                        , class "jumbo-image"
+                        ]
+                        []
+                    , h2
                         []
                         [ text "It's Ballanced" ]
                     , p
@@ -83,16 +116,24 @@ view model =
                             "Candidates will only get the electoral votes that they deserve. Candidates will have to appeal to all voters, not just "
                                 ++ "those who live in a couple of swing states."
                         ]
+                    , license "White House" "https://www.flickr.com/photos/16698683@N00/15870725062" "Diego Cambiaso" "https://www.flickr.com/photos/16698683@N00"
                     ]
                 , div
                     [ class "col-md-4" ]
-                    [ h2
+                    [ img
+                        [ style "display" "block"
+                        , src "src/img/pr_map.svg"
+                        , alt "White House"
+                        , class "jumbo-image"
+                        ]
+                        []
+                    , h2
                         []
                         [ text "It's Tested" ]
                     , p
                         []
                         [ text <|
-                            "Nearly 100 countries use Proportional Representation. It allows all factions to be represented, and prevents a monopoly "
+                            "Over 100 countries use Proportional Representation. It allows all factions to be represented, and prevents a monopoly "
                                 ++ "on power by a single party."
                         ]
                     ]
