951ee407ca48bfcc6f6e3d2765ccb279842b2ac1
KingWither <67877704+KingWither@users.noreply.github.com>
2021-11-19 15:09:11 -0500

    Fixed compile directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,15 +15,16 @@ scss error.scss notauthorized.css
 
 cd ../../pages
 
-python -u compile.py PageNotFound.elm
-python -u compile.py NotAuthorized.elm
-python -u compile.py Calculator.elm
-python -u compile.py CountMeIn.elm
-python -u compile.py Donate.elm
-python -u compile.py DonateFAQ.elm
-python -u compile.py Endorsements.elm
-python -u compile.py Index.elm
-python -u compile.py Map.elm
-python -u compile.py Proposal.elm
-python -u compile.py ReadMore.elm
-python -u compile.py StateResults.elm
+python -u ../compile.py PageNotFound.elm
+python -u ../compile.py NotAuthorized.elm
+python -u ../compile.py Calculator.elm
+python -u ../compile.py CountMeIn.elm
+python -u ../compile.py Donate.elm
+python -u ../compile.py DonateFAQ.elm
+python -u ../compile.py Endorsements.elm
+python -u ../compile.py Index.elm
+python -u ../compile.py Map.elm
+python -u ../compile.py Proposal.elm
+python -u ../compile.py ReadMore.elm
+python -u ../compile.py StateResults.elm
+python -u ../compile.py Unsubscribe.elm
