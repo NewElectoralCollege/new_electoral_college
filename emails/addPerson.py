4076fe2d01e98d8d8c588753a92c0034cfdf0ce4
KingWither <67877704+KingWither@users.noreply.github.com>
2021-11-19 13:05:59 -0500

    Fixed emails

--- emails/addPerson.py
+++ emails/addPerson.py
@@ -1,28 +1,31 @@
-#!C:\Python27\python
+#!/usr/bin/python
 
-from emails import send
 import csv
 import cgi
-print("Content-Type: text/html; charset=utf-8\n\n")
 
+from emails import send
+
+print("Content-Type: text/html; charset=utf-8\n\n")
 
 form = cgi.FieldStorage()
 
-list = ["False"]
+form_result = ["False"]
 
 for term in ["first-name", "last-name", "state", "email"]:
-    list.insert(len(list)-1, form.getvalue(term))
+    form_result.insert(len(form_result)-1, form.getvalue(term))
 
 with open("emails.csv", "r") as file:
     lines = csv.reader(file)
+
     for first, last, state, email, admin in lines:
-        if email == list[3]:
+        if email == form_result[3]:
             exit(-1)
+    
 
 file.close()
 
 file = open("emails.csv", "a")
-file.write("\n" + ",".join(list))
+file.write("\n" + ",".join(form_result))
 file.close()
 
-send("Thank you for Signing Up for the Newsletter!", "/welcome_email.html", list)
+send("Thank you for Signing Up for the Newsletter!", "welcome_email.html", form_result)
